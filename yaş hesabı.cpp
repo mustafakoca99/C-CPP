af20667b71981eb418f965297f8a54255f24b0a8
Mustafa Koca <54147948+mustafakoca99@users.noreply.github.com>
2019-08-31 21:11:46 +0300

    Add files via upload

---
+++ yaş hesabı.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+	int s1,s2,toplam;
+	printf("bu yili yaziniz:");
+	scanf("%d",&s2);
+	printf("dogum yilinizi yaziniz:");
+	scanf("%d",&s1);
+	toplam=s2-s1;
+	printf("toplam=%d",toplam);
+}
