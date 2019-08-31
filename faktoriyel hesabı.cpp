41b364dd94ccb70151dee169be3d520d86e3fbb1
Mustafa Koca <54147948+mustafakoca99@users.noreply.github.com>
2019-08-31 21:09:27 +0300

    Add files via upload

---
+++ faktoriyel hesabı.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+int i,n;
+double top,ort;
+	top=top+i;
+	ort=top/n;
+for(i=0; i<=5; i++)
+{
+	printf("kac adet sayi:");
+	scanf("%d",&n);
+}	
+	printf("sayi:",n);
+printf("toplama=%lf",top);
+printf("ortalama=%lf",ort);
+}
+
+
