52b085655ef8be1f98c7309a80916fbbc402b2ec
Mustafa Koca <54147948+mustafakoca99@users.noreply.github.com>
2019-08-31 20:51:46 +0300

    Add files via upload

---
+++ asal sayi kontrol.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int asal_mi(int sayi)
+{
+	int i;
+	for(i=2; i<sayi; i++)
+	{
+		if(sayi%i==0)
+		{
+			return 0;
+		}
+	}
+}
+int main ()
+{
+	int n;
+	printf("bir sayi gir:");
+	scanf("%d",&n);
+	if(asal_mi(n)==0)
+	{
+			printf("bu sayi asal sayi degildir");
+	}
+	else
+	{
+			printf("bu sayi asal sayidir");
+	}
+}
